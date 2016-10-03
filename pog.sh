04c48ef697c174231694a9168c645d44760c8d74
Anastasiya <anastkononova@gmail.com>
2016-10-03 15:32:06 +0300

    Commit3

--- pog.sh
+++ pog.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 while [ TRUE ]; do
 rm minsk
-wget https://yandex.by/pogoda/minsk
-clear 
+wget -q https://yandex.by/pogoda/minsk
 grep -m 1 -o '<div class="current-weather__thermometer current-weather__thermometer_type_now">.*C</div></span>' minsk | egrep -o '[+-]*[[:digit:]]+'
 sleep 3
 done
